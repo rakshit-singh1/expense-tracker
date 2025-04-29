85dfb443e08664adf4374731fcf0a0a9a88e1529
rakshit-singh2 <thisisjames007@gmail.com>
2025-04-29 13:00:36 +0530

    correction

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ExpenseDate from './ExpenseDate.js';
-import Card from '../UI/Card.js'; 
+import ExpenseDate from './ExpenseDate.jsx';
+import Card from '../UI/Card.jsx'; 
 import './ExpenseItem.css';
 const ExpenseItem = (props) => {
     // const expenseDate= new Date(2021, 2, 28);
